8fd422e8305271f242e354bec0f8f170df22485f
Soumyodeep-Das <linux.programmer.06@gmail.com>
2024-09-24 22:36:57 +0530

    Adding styling and image

--- 01-app/src/App.jsx
+++ 01-app/src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import "./App.css";
 
 function App() {
     return (
-        <h1>My First React Application</h1>
+        <div className="container">
+            <h1 className="header">My First React Application</h1>
+            <img src="/logo192.png" alt="react logo" />
+        </div>
     )
 }
+
 
 export default App
